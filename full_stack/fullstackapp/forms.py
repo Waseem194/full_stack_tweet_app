035b36402060e3a004428384f53ff869b7df4a1e
Waseem194 <bsf1801678@ue.edu.pk>
2024-06-23 02:04:58 +0500

    formcreations

--- full_stack/fullstackapp/forms.py
+++ full_stack/fullstackapp/forms.py
@@ -6,3 +6,9 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ["text","photo"]
+        
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'})
+    
+    photo = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'})
+    )
